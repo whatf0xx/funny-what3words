894a416ad98b2d90955a7da9bf199db7c790905a
whatf0xx <hfox022@gmail.com>
2023-10-20 13:03:41 +0200

    Working 'Hello World' of this dumb project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import random
 import what3words
-from secrets import WHAT3WORDS_API_KEY
+import openai
+from secrets import WHAT3WORDS_API_KEY, OPENAI_API_KEY
 
 if __name__ == '__main__':
     x = (random() - 0.5) * 180
@@ -11,3 +12,16 @@ if __name__ == '__main__':
     package = geocoder.convert_to_3wa(coords)
     words, location = package["words"], package["coordinates"]
     print(f"Got {words} for a spot at {location}!")
+
+    openai.api_key = OPENAI_API_KEY
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system",
+             "content": "You run a humorous meme page and are skilled at finding witty word plays."},
+            {"role": "user",
+             "content": f"In a line or two, reason whether or not the word combination {words} is funny."}
+        ]
+    )
+
+    print(completion.choices[0].message["content"])
